e8a6743c2432db1e501f3d3e7d6072205d1823f6
hoon <moradulee@gmail.com>
2021-10-17 16:52:03 +0900

    #1 불필요한 의존성을 제거하고 자율적인 존재로 설계 개선하기

--- src/main/java/ch01/domain/Theater.java
+++ src/main/java/ch01/domain/Theater.java
@@ -11,22 +11,7 @@ public class Theater {
         this.ticketSeller = ticketSeller;
     }
 
-    /**
-     * 소극장은 먼저 관람객 가방 안에 초대장이 들어있는지 확인한다.
-     * 들어 있다면 이벤트에 당첨된 관객이므로 판매원에게서 받은 티켓을 관람객 가방 안에 넣어준다.
-     * 만약 초대장이 없다면, 티켓을 판매해야 한다.
-     * 이 경우 소극장은 관람객 가방에서 티켓 금액 만큼 돈을 차감한 후에 매표소 금액을 증가 시킨다.
-     * 그 다음, 소극장은 관람객 가방에 티켓을 넣어줌으로써 입장 절차는 끝난다.
-     */
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
